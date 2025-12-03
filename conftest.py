13d4757288000fe52255218678e7214eec6b884d
jenualdo-1982 <jenualdo@gmail.com>
2025-12-03 17:31:11 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,12 +4,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 @pytest.fixture(scope="function")
-def driver(request):  # ← добавляем request
+def driver(request): 
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
 
-    # Скриншот только если тест упал
     if request.node.rep_call.failed if hasattr(request.node, "rep_call") else False:
         allure.attach(
             driver.get_screenshot_as_png(),
